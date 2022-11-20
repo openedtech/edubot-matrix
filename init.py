d5b6cab5f4adaedd7e6712377f261fb90f7b7f20
exciteabletom <tom@digitalnook.net>
2022-11-20 19:16:22 +0800

    init

---
+++ init.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import asyncio
+
+try:
+    from moodlebot import main
+
+    # Run the main function of the edubot
+    asyncio.get_event_loop().run_until_complete(main.main())
+except ImportError as e:
+    print("Unable to import moodlebot.main:", e)
---
+++ matrix/g.py
@@ -0,0 +1,5 @@
+from typing import Union
+
+from matrix.config import Config
+
+config: Union[Config, None] = None
